54630bee3b41ccd49df18097fb144bd1a4011df6
Josh Nussbaum <joshnuss@gmail.com>
2021-11-15 13:28:42 -0500

    Adds var for host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :elixir_vite_app, ElixirViteAppWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOST"), port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
